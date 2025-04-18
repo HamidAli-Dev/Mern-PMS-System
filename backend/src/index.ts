1c2e545f15778da3532be4b074daa3d03e31e052
Hamid Ali <hamidali.webdev@gmail.com>
2025-04-18 11:30:12 +0500

    feat: initialize backend with TypeScript, Express, and MongoDB configuration

---
+++ backend/src/index.ts
@@ -0,0 +1,43 @@
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import session from "cookie-session";
+import cors from "cors";
+
+import { config } from "./config/app.config";
+import connectDB from "./config/db.config";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    name: "session",
+    keys: [config.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000,
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: "lax",
+  })
+);
+
+app.use(
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(config.PORT, async () => {
+  console.log(
+    `Server is running on port ${config.PORT} in ${config.NODE_ENV} mode`
+  );
+  await connectDB();
+});
