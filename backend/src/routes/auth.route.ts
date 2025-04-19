b0969b1ac1406bd299730f42e6087e8ca75ab8d6
Hamid Ali <hamidali.webdev@gmail.com>
2025-04-19 11:03:23 +0500

    implement local authentication strategy and user login/logout functionality

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -4,6 +4,8 @@ import passport from "passport";
 import { config } from "../config/app.config";
 import {
   googleLoginCallback,
+  loginController,
+  logOutController,
   registerUserController,
 } from "../controllers/auth.controller";
 
@@ -12,6 +14,9 @@ const failedUrl = `${config.FRONTEND_GOOGLE_CALLBACK_URL}?status=failure`;
 const authRoutes = Router();
 
 authRoutes.post("/register", registerUserController);
+authRoutes.post("/login", loginController);
+
+authRoutes.post("/logout", logOutController);
 
 authRoutes.get(
   "/google",
