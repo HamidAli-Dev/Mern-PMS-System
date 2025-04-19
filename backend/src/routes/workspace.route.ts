995e02b86d69ecd4d1a68ed53ca3ec0da9d1102e
Hamid Ali <hamidali.webdev@gmail.com>
2025-04-19 16:09:06 +0500

    implement workspace and member management features with join and create functionality

---
+++ backend/src/routes/workspace.route.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+
+import {
+  changeWorkspaceMemberRoleController,
+  createWorkspaceController,
+  deleteWorkspaceByIdController,
+  getAllWorkspacesUserIsMemberController,
+  getWorkspaceAnalyticsController,
+  getWorkspaceByIdController,
+  getWorkspaceMembersController,
+  updateWorkspaceByIdController,
+} from "../controllers/workspace.controller";
+
+const workspaceRoutes = Router();
+
+workspaceRoutes.post("/create/new", createWorkspaceController);
+
+workspaceRoutes.get("/all", getAllWorkspacesUserIsMemberController);
+
+workspaceRoutes.get("/:id", getWorkspaceByIdController);
+
+workspaceRoutes.get("/members/:id", getWorkspaceMembersController);
+workspaceRoutes.get("/analytics/:id", getWorkspaceAnalyticsController);
+
+workspaceRoutes.put(
+  "/change/member/role/:id",
+  changeWorkspaceMemberRoleController
+);
+
+workspaceRoutes.put("/update/:id", updateWorkspaceByIdController); // Update workspace by ID
+
+workspaceRoutes.delete("/delete/:id", deleteWorkspaceByIdController);
+
+export default workspaceRoutes;
